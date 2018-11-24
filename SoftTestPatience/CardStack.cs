7481c14834999b950f443f280c43cdf13fe13c71
DESKTOP-C16OKR5\Dennis <dennisstiekema@gmail.com>
2018-11-24 11:13:10 +0100

    REFACTOR Add CardStack class

---
+++ SoftTestPatience/CardStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftTestPatience
+{
+    abstract class CardStack
+    {
+        private List<Card> cards;
+
+        protected CardStack(List<Card> cards)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Card GetCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual int GetStackSize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
