8d980044ad54c883c19fef9e267decf844acdaa5
DESKTOP-C16OKR5\Dennis <dennisstiekema@gmail.com>
2018-11-24 18:29:15 +0100

    REFACTOR Add GameController class

---
+++ SoftTestPatience/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftTestPatience
+{
+    // Placeholder
+    class Board { }
+
+    class GameController
+    {
+        private Board table;
+
+        public GameController(Board table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NewGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUserInput()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
