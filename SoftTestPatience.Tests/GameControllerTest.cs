163535eae0ab9090101c5d3f16bc1014a8e6edb3
DESKTOP-C16OKR5\Dennis <dennisstiekema@gmail.com>
2018-11-25 12:10:26 +0100

    RED Add test to see if ManageUserInput() returns the used input

--- SoftTestPatience.Tests/GameControllerTest.cs
+++ SoftTestPatience.Tests/GameControllerTest.cs
@@ -25,5 +25,25 @@ namespace SoftTestPatience.Tests
                 Assert.Equal(expected, sw.ToString());
             }
         }
+
+        [Fact]
+        public void ManageUserInput_MoveStackOf3Cards_ShouldReturnMoveInStringFormat()
+        {
+            Mock<Board> mock = new Mock<Board>();
+            GameController sut = new GameController(mock.Object);
+
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                using (StringReader sr = new StringReader("5 7 3"))
+                {
+                    Console.SetIn(sr);
+                    string actual = sut.ManageUserInput();
+                    string expected = "5 7 3";
+                    Assert.Equal(expected, actual);
+                }
+            }
+        }
     }
 }
