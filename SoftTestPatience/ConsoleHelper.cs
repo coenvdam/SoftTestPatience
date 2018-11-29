2e743129da819fea1d8d64f9d3a293fc82305d3d
coenvdam <coenvdam@live.nl>
2018-11-29 12:33:33 +0100

    REFACTOR Added ConsoleHelper

---
+++ SoftTestPatience/ConsoleHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftTestPatience
+{
+    interface IConsoleHelper
+    {
+        void SetEncoding();
+        void Write();
+        string ReadInput();
+    }
+
+    class ConsoleHelper : IConsoleHelper
+    {
+        public void SetEncoding()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReadInput()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
